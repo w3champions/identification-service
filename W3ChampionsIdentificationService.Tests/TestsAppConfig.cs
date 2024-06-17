87bd83e39abf04ca02354e49f86b5a7e387c6976
konstantin.zavadsky <konstantin.zavadsky@semrush.com>
2024-06-17 20:23:09 +0200

    return old mongo string

--- W3ChampionsIdentificationService.Tests/TestsAppConfig.cs
+++ W3ChampionsIdentificationService.Tests/TestsAppConfig.cs
@@ -4,7 +4,7 @@ namespace W3ChampionsIdentificationService.Tests
 {
     public class TestsAppConfig : IAppConfig
     {
-        public string MongoConnectionString => "mongodb://localhost:27017"; // "mongodb://localhost:27017";
+        public string MongoConnectionString => "mongodb://157.90.1.251:3712"; // "mongodb://localhost:27017";
 
         public string DatabaseName => "W3Champions-Identification-Service-Tests";
         
